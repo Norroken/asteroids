7fae49995435f3679d8469861f319c9b1d9b8a04
Norroken <Verusinsaniaspecto@gmail.com>
2025-02-19 19:18:26 +0000

    Using import functions

---
+++ main.py
@@ -0,0 +1,19 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+from constants import *
+
+# Main function 
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+
+# ensures main function is only called when the file is run directly 
+if __name__ == "__main__":
+    main()
+
