ee47cf07da8188e17c0858bc700dd38fba467ffb
Norroken <Verusinsaniaspecto@gmail.com>
2025-02-21 20:18:05 +0000

    Added Player Sprite with rotation

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame
+import pygame           #Imports pygame module
+import player
 
-from constants import *
+from constants import * #import in variable constants from constants.py
 
 # Main function 
 def main():
-    pygame.init()
-    #print("Starting asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()   #initiate pygame when the program is run
+    fps = pygame.time.Clock()
+    dt = 0
     
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #define screen size by the constants
+    player_sprite = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
+    
+    # Main infinite loop that is the body of the game, 
     while True:
+        #Event handler for quitting the game
         for event in pygame.event.get():
             if event.type  == pygame.QUIT:
                 return
+
         
-        print("Loop running!")
-        screen.fill("black")
-        pygame.display.flip()
+        screen.fill("black") #Sets screen background to black
+        player_sprite.draw(screen) #Invokes player every frame
+        player_sprite.update(dt)
+        pygame.display.flip() #refreshes the scene every loop
+        dt = (fps.tick(60)/1000)
         
 
 
